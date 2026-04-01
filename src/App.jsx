d23c445c545d7e72c7cb3b24d348f7d0801ff6f7
sangram.p <sangram.panda@iserveu.co.in>
2026-04-01 09:47:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,18 @@ function App() {
   return (
     <>
     <div className="App">
+      {/* excersize 1 for using prop */}
       <Button label= "Success" color="green"></Button>
       <Button label= "Delete" color="red"></Button>
+      {/* Excersize 2 for Toggle using useState*/}
       <Toggle/>
+      {/* Excersize 3 for HoverBox using transition */}
       <HoverBox/>
+      {/* Excersize 4 for HoverBox using Conditional Rendering */}
       <TrafficLight color="red" />
       <TrafficLight color="yellow" />
       <TrafficLight color="green" />
+      {/* Excersize 5 for TodoList using map to iterate the data using key */}
       <TodoList/>
     </div>
     </>
